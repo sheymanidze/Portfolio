22fe81b15241b9329c94b88ff79ee9cba6aca165
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-07-08 13:41:23 -0400

    particles js added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './components/Navbar'
-import Header from './components/Header'
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Particles from "react-particles-js";
 
 function App() {
   return (
     <>
-   <Navbar/>
-   <Header/>
+     <Particles
+     params={{
+       particles: {
+         number: {
+           value:30,
+           density: {
+             enable: true, 
+             value_area: 900
+           }
+
+         }
+
+       }
+     }}/>
+     <Navbar/>
+     <Header/>
    </>
   );
 }

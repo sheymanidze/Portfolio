9f3e44acdbd1c24f58698171bba5bce6dd970c67
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-07-09 17:22:52 -0400

    social media btn added, linked

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,13 @@
 import React from "react";
 import {Link} from "react-scroll";
+import {
+  EmailShareButton,
+  EmailIcon,
+  LinkedinShareButton,
+  LinkedinIcon
+} from "react-share";
+import { FaGithubSquare } from "react-icons/fa";
+
 
 
 const Footer = () => {
@@ -11,14 +19,8 @@ const Footer = () => {
             <div className="d-flex">
             <a href="https://github.com/sheymanidze?tab=repositories" target="_blank">Github</a>
             </div>
-            <div className="d-flex">
-              <div className="emailFooter">
-               <a href="mailto:sorkin.anastasia@gmail.com">sorkin.anastasia@gmail.com</a>
-            </div>
-              
-            </div>
           </div>
-          <div className="webSections col-lg-2 col-md-2 col-sm-6">
+          <div className="webSections col-lg-4 col-md-2 col-sm-6">
             <div className="row">
               <div className="col">
                 <Link smooth={true} to="home" offset={-110} className="footer-nav">Home</Link>
@@ -33,8 +35,31 @@ const Footer = () => {
               </div>
             </div>
           </div>
+
+          <div className="socialMediaBtn d-flex justify-content-end">
+           <div className="col-lg-3 col-md-5 col-sm-6 pt-3 text-center">
+             <LinkedinShareButton
+                url={"https://www.linkedin.com/in/sheymanidze/"}
+                quote={"FullStack Developer"}
+                hashtag="#javascript"
+              >
+                <LinkedinIcon className="mx-3" size={36} />
+              </LinkedinShareButton>
+              <EmailShareButton
+                url={"mailto:sorkin.anastasia@gmail.com"}
+              >
+                <EmailIcon className="mx-3" size={36} />
+              </EmailShareButton>
+              </div>
+              
+              <div
+                url={"https://github.com/sheymanidze?tab=repositories"}>
+                <FaGithubSquare className="mx-3" size={36} />
+              </div>
+              </div>
           <div className="copyRights align-items-center">
             <p className="pt-3 text-center">
+              
               Copyright&copy;
               {new Date().getFullYear()}&nbsp;Anastasia Sorkin | Full Stack Developer
             </p>

4a9a76470fad170f2a803cea6f15f1eae936a2ac
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-07-09 15:01:03 -0400

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from "react";
+
 
 const Footer = () => {
   return (
@@ -7,14 +8,38 @@ const Footer = () => {
         <div className="row">
           <div className="col-lg-4 col-md-6 col-sm-6">
             <div className="d-flex">
-
+            <a href="https://github.com/sheymanidze?tab=repositories" target="_blank">Github</a>
             </div>
+            <div className="d-flex">
+              <p>sorkin.anastasia@gmail.com</p>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-2 col-sm-6">
+            <div className="row">
+              <div className="col">
+                <a className="footer-nav">Home</a>
+                <br />
+                <a className="footer-nav">About me</a>
+                <br />
+                <a className="footer-nav">Portfolio</a>
+              </div>
+              <div className="col">
+                <a className="footer-nav">Resume</a>
+                <br />
+                <a className="footer-nav">Contact Me</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-5 col-md-5 col-sm-6 align-items-center">
+            <p className="pt-3 text-center">
+              Copyright&copy;
+              {new Date().getFullYear()}&nbsp;Anastasia Sorkin | All Rights Reserved
+            </p>
           </div>
         </div>
       </div>
-      
     </div>
   )
 }
 
-export default Footer
+export default Footer;

6fbce06cc9862a4ebceba9afaddac240f04e918b
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-07-10 09:32:45 -0400

    footer styling fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,25 +15,8 @@ const Footer = () => {
     <div className="footer">
       <div className="container">
         <div className="row">
-          <div className="webSections col-lg-4 col-md-2 col-sm-6">
-            <div className="row">
-              <div className="col">
-                <Link smooth={true} to="home" offset={-110} className="footer-nav">Home</Link>
-                <br />
-                <Link smooth={true} to="aboutMe" offset={-110} className="footer-nav">About me</Link>
-                <br />
-              </div>
-              <div className="col">
-              <Link smooth={true} to="portfolio" offset={-110} className="footer-nav">Portfolio</Link>
-                <br />
-                <Link smooth={true} to="contactMe" offset={-110} className="footer-nav">Contact Me</Link>
-              </div>
-            </div>
-          </div>
-
-          <div className="socialMediaBtn d-flex justify-content-end">
-           <div className="col-lg-3 col-md-5 col-sm-6 pt-3 text-center">
-             <LinkedinShareButton
+        <div class="col">
+              <LinkedinShareButton
                 url={"https://www.linkedin.com/in/sheymanidze/"}
                 quote={"FullStack Developer"}
                 hashtag="#javascript"
@@ -45,15 +28,35 @@ const Footer = () => {
               >
                 <EmailIcon className="mx-3" size={36} />
               </EmailShareButton>
-              </div>
+              
               
               <div
                 url={"https://github.com/sheymanidze?tab=repositories"}>
                 <FaGithubSquare className="mx-3" size={36} />
+
+              </div>
+              </div>
+              <div className="col-md-auto">
+                <Link smooth={true} to="home" offset={-110} className="footer-nav">Home</Link>
+                <br />
+                <Link smooth={true} to="aboutMe" offset={-110} className="footer-nav">About me</Link>
+                <br />
+              </div>
+         
+              <div className="col col-lg-2">
+              <Link smooth={true} to="portfolio" offset={-110} className="footer-nav">Portfolio</Link>
+                <br />
+                <Link smooth={true} to="contactMe" offset={-110} className="footer-nav ">Contact Me</Link>
+              </div>
+          
+
+          <div className="socialMediaBtn d-flex justify-content-end">
+           <div className="col-lg-3 col-md-5 col-sm-6 pt-3 text-center">
+           
               </div>
               </div>
           <div className="copyRights align-items-center">
-            <p className="pt-3 text-center">
+            <p className="pt-3 text-center copyright">
               
               Copyright&copy;
               {new Date().getFullYear()}&nbsp;Anastasia Sorkin | Full Stack Developer

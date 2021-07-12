93eeb1003ac31a347b8c4ce4878087b1c9cbaa08
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-07-12 19:49:44 -0400

    footer fixed, logo home link added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../logo.png";
 import {Link} from "react-scroll";
+import resume from "../images/Resume.pdf"
 
 //react font awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,7 +13,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
       <div className="container">
 
-    <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo"></img></a>
+    <Link smooth={true} to="home" className="navbar-brand"><img className="logo" src={logo} alt="logo"></img></Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <FontAwesomeIcon icon={faBars} style ={{color: "#fff"}}/>
     </button>
@@ -20,16 +21,19 @@ const Navbar = () => {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ml-auto">
         <li className="nav-item active">
-          <Link smooth={true} to="home" className="nav-link" href="#">Home <span className="sr-only"></span></Link>
+          <Link smooth={true} to="home" className="nav-link">Home <span className="sr-only"></span></Link>
         </li>
         <li className="nav-item">
-          <Link smooth={true} to="aboutMe" offset={-110} className="nav-link" href="#">about me</Link>
+          <Link smooth={true} to="aboutMe" offset={-110} className="nav-link">about me</Link>
         </li> 
         <li className="nav-item">
-          <Link smooth={true} to="portfolio" offset={-110} className="nav-link" href="#">portfolio</Link>
+          <Link smooth={true} to="portfolio" offset={-110} className="nav-link">portfolio</Link>
         </li> 
+        <li className="nav-item active">
+          <a className="nav-link" href={resume} target="_blank" rel="noopener noreferrer">Resume <span className="sr-only"></span></a>
+        </li>
         <li className="nav-item">
-          <Link smooth={true} to="contactMe" offset={-110} className="nav-link" href="#">contact me</Link>
+          <Link smooth={true} to="contactMe" offset={-110} className="nav-link">contact me</Link>
         </li> 
       </ul>
     </div>
